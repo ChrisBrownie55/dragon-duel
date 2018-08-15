ca8d1ce48fdfef704606173bdce653ce539f0ddc
Christopher Brown <chbphone55@gmail.com>
2018-08-15 14:07:54 -0600

    main.js and some renaming

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
+import CharacterSelectController from './components/character-select/character-select-controller.js';
+import BattleController from './components/battle/battle-controller.js';
+
 class App {
   constructor() {
-    this.controllers = {};
+    this.controllers = {
+      characterSelect: new CharacterSelectController(),
+      battle: null
+    };
+  }
+
+  createBattle(championID, dragonID) {
+    this.controllers.battle = new BattleController(championID, dragonID);
+  }
+  endBattle() {
+    this.controllers.battle = null;
+    this.controllers.characterSelect.show();
   }
 }
 
